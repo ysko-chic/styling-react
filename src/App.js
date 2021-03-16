d899578f67ced5321df390db3d70c5b0dda56b3c
yskochic <ellese0916@gmail.com>
2021-03-16 14:28:55 +0900

    yarn add classnames

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
 
 class App extends Component {
   render() {
     return(
-      <SassComponent />
+      // <SassComponent />
+      <CSSModule />
     )
   }
 }
